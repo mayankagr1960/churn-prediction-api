ae58c7520b08e7744528f51310594b2f3b5611e4
Mayank Agrawal <mayank.agr1960@gmail.com>
2026-01-11 15:07:01 +0100

    Initial Commit - Churn prediction model

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,84 @@
+# 📊 Customer Churn Prediction – End-to-End Machine Learning Project
+
+## 📌 Problem Statement
+Customer churn is a critical challenge for subscription-based businesses.  
+The objective of this project is to **predict whether a customer will churn** and **identify the key drivers behind churn**, enabling businesses to take proactive retention actions.
+
+This project demonstrates a **full machine learning lifecycle**, from data exploration to deployment.
+
+---
+
+## 📂 Dataset
+- **Telco Customer Churn Dataset**
+- ~7,000 customer records
+- Mix of numerical and categorical features:
+  - Tenure
+  - Monthly & total charges
+  - Contract type
+  - Payment method
+  - Gender
+  - Customer churn (target)
+
+---
+
+## ⚙️ Project Workflow
+
+1. Exploratory Data Analysis (EDA)
+2. Data preprocessing & feature engineering
+3. Train-Test Split
+4. Baseline modeling (Logistic Regression)
+5. Advanced modeling (Random Forest)
+6. Key metrics evaluation
+7. Hyperparameter tuning (GridSearchCV)
+8. Model explainability (SHAP)
+9. Import and export of model
+10. API deployment using FastAPI
+
+---
+
+## 🧠 Models Used
+- Logistic Regression – baseline & interpretability
+- Random Forest (Tuned) – Final production model
+
+---
+
+## 📈 Model Performance
+
+Class	Presicion	Recall	F1-Score
+0	    0.91	    0.77	0.83
+1	    0.54	    0.76	0.63
+Overall Accuracy			0.77
+ROC-AUC                     0.85
+
+
+### Business Interpretation
+- The model achieves an overall accuracy of **77%**, indicating solid general performance on unseen data.
+- For Class 0 (Non-Churn), the model shows high precision **(0.91)** and a strong F1-score **(0.83)**, meaning it reliably identifies non-churning customers.
+- For Class 1 (Churn), the model attains a recall of **0.76**, showing it successfully captures most churn cases, which is critical for retention strategies.
+- The F1-score of **0.63** for churners reflects a balanced trade-off between precision and recall in an imbalanced dataset.
+- With a ROC-AUC of **0.85** and **77%** overall accuracy, the model reliably distinguishes between churn and non-churn customers, supporting **data-driven decision-making**.
+
+
+### Business Actions
+- The model helps the business identify customers likely to leave early, allowing teams to take timely retention actions.
+- The predictions are reliable and consistent, giving confidence in using the model for customer-focused decisions.
+
+---
+
+## 🔍 Key Churn Drivers (SHAP Explainability)
+- Low customer tenure
+- Month-to-month contracts
+- High monthly charges
+- High total charges
+- Lack of long-term commitment
+- Senior and non-senior citizens
+
+SHAP was used to explain:
+- Global feature importance
+- Individual customer predictions
+
+---
+
+## 🚀 Live API Deployment
+
+The trained model is deployed as a REST API using FastAPI.
---
+++ app.py
@@ -0,0 +1,72 @@
+##Real-time Prediction via API
+
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+import joblib
+import pandas as pd
+
+#Load Model
+model = joblib.load('final_churn_model.pkl')
+
+#Define Basemodel
+class Table_Schema(BaseModel):
+    gender:                                     int
+    SeniorCitizen:                              int
+    Partner:                                    int
+    Dependents:                                 int
+    tenure:                                   float
+    PhoneService:                               int
+    MultipleLines:                              int
+    OnlineSecurity:                             int
+    OnlineBackup:                               int
+    DeviceProtection:                           int
+    TechSupport:                                int
+    StreamingTV:                                int
+    StreamingMovies:                            int
+    PaperlessBilling:                           int
+    MonthlyCharges:                           float
+    TotalCharges:                             float
+    InternetService_Fiber_optic:                int = Field(..., alias='InternetService_Fiber optic')
+    InternetService_No:                         int
+    Contract_One_year:                          int = Field(..., alias='Contract_One year')
+    Contract_Two_year:                          int = Field(..., alias='Contract_Two year')
+    PaymentMethod_Credit_card:                  int = Field(..., alias='PaymentMethod_Credit card (automatic)')
+    PaymentMethod_Electronic_check:             int = Field(..., alias='PaymentMethod_Electronic check')
+    PaymentMethod_Mailed_check:                 int = Field(..., alias='PaymentMethod_Mailed check')
+    tenure_group:                               int
+
+#Allow using Python names internally
+#class Config:
+ #   allow_population_by_field_name = True
+  #  allow_population_by_alias = True
+       
+#Initialize API
+app = FastAPI()
+
+#Get Endpoint
+@app.get("/")
+def read_root():
+    return('Churn model is ready for prediction. Please provide customer details..!!')
+
+#Predict Endpoint
+@app.post('/predict')
+def predict_post(data: Table_Schema):
+    #Convert input data to dataframe
+    indata = pd.DataFrame([data.dict(by_alias=True)])
+
+    #Prediction using ML Model
+    #[:,1]Predict probablity for +ve class 1(Churn = True)
+    #[0]Extract Scalar value
+    churn_proba = model.predict_proba(indata)[:,1][0]
+    churn_proba_01 = churn_proba * 100
+
+    #Convert probablity to prediction value, with 0.5 threshold
+    #int to convert True/False to 1/0
+    churn_pred = int(churn_proba > 0.5)
+    return {f"Churn probability of a customer: {churn_proba_01:.2f}%", 
+            f"Churn prediction by Model: {churn_pred}",
+            f"{'Customer will likely churn.' if churn_pred == 1 else 'Customer will likely stay.'}"
+            }
+
+# Load batch data (For batch processing)
+#indata = pd.read_csv("batch_customers.csv")
